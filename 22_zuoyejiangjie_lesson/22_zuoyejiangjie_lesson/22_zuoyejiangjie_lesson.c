34cb852c605ab91c8fe0b86f34dfc592aae7d6f7
SP810 <2451142030@qq.com>
2022-05-17 22:16:40 +0800

    22节结束

--- 22_zuoyejiangjie_lesson/22_zuoyejiangjie_lesson/22_zuoyejiangjie_lesson.c
+++ 22_zuoyejiangjie_lesson/22_zuoyejiangjie_lesson/22_zuoyejiangjie_lesson.c
@@ -364,14 +364,101 @@
 //}
 // 
 // 
+// 有序序列合并
+// 
+//#include<stdio.h>
+//int main()
+//{
+//    int n;
+//    int m;
+//    int arr1[1000] = { 0 };
+//    int arr2[1000] = { 0 };
+//    int arr3[1000] = { 0 };
+//    scanf("%d %d", &n, &m);
+//    int i = 0;
+//    int j = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr1[i]);
+//        arr3[i] = arr1[i];
+//    }
+//    for (j = 0; j < m; j++)
+//    {
+//        scanf("%d", &arr2[j]);
+//        arr3[n + j] = arr2[j];
+//    }
+//    for (i = 0; i < n + m; i++)
+//    {
+//        for (j = 1; j < n + m - i; j++)
+//        {
+//            if (arr3[j - 1] > arr3[j])
+//            {
+//                int tmp = arr3[j - 1];
+//                arr3[j - 1] = arr3[j];
+//                arr3[j] = tmp;
+//            }
+//        }
+//    }
+//    for (i = 0; i < n + m; i++)
+//        printf("%d ", arr3[i]);
+//    return 0;
+//}
 // 
 // 
 // 
-// 
-// 
-// 
-// 
-// 
+// 参考答案版
+//#include <stdio.h>
+//
+//
+//int main()
+//{
+//    int n = 0;
+//    int m = 0;
+//    int arr1[100] = { 0 };
+//    int arr2[100] = { 0 };
+//    //输入
+//    scanf("%d %d", &n, &m);
+//    int i = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr1[i]);
+//    }
+//    for (i = 0; i < m; i++)
+//    {
+//        scanf("%d", &arr2[i]);
+//    }
+//    //处理
+//    int j = 0;
+//    i = 0;
+//    while (i < n && j < m)
+//    {
+//        if (arr1[i] < arr2[j])
+//        {
+//            printf("%d ", arr1[i]);
+//            i++;
+//        }
+//        else
+//        {
+//            printf("%d ", arr2[j]);
+//            j++;
+//        }
+//    }
+//    if (i == n)
+//    {
+//        for (; j < m; j++)
+//        {
+//            printf("%d ", arr2[j]);
+//        }
+//    }
+//    else
+//    {
+//        for (; i < n; i++)
+//        {
+//            printf("%d ", arr1[i]);
+//        }
+//    }
+//    return 0;
+//}
 // 
 // 
 // 
