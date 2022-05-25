9b8efcf40f334d105f63d42be45765d9ba50f5ee
SP810 <2451142030@qq.com>
2022-05-25 18:09:26 +0800

    第五章

---
+++ 5_1.py
@@ -0,0 +1,46 @@
+# cars=['audi','bmw','toyota','rover']
+# for a in cars:
+#     if a=='rover':##if的使用
+#         # print(a.upper())
+#         b=f"{a.upper()}haihiahia"#浅浅复习一下f吧
+#         print(b)
+#print(f"{a.upper()}haihiahia")#或者这么写
+#     else:
+#         print(a.title())
+##
+##5.2  条件测试：
+# #== 相等  ！=   不相等
+#在python里面，&&并且就是 and   ,||或者 就是   or
+##检查特定值
+## 关键词in
+## 如果我们想要调查某个元素  能否等于那个玩意儿
+## 例如 想要知道a 在不在字母列表当中 ，我们可以用in查询  ,同样，还有not in
+# zimu=['a','bda','asd','asda']
+# if 'a' in zimu:
+#     print("haihai")
+##5.3  if-elif-else 结构
+# age=12
+# if age<4:
+#     print("free")
+# elif age<12:
+#     print("halef price")
+# else:
+#     print("原价，木商量")
+##然后剩下的用法其实也和原先差不多啦，就是if if  和if elif 的区别
+##分条件合理使用好么，答应我宝贝
+##5.4 用if确定列表是否空不空
+# liebiao=[]
+# if liebiao:
+#     print("那应该是有东西")
+# else:
+#     print("啥也没有")
+
+
+
+
+
+
+
+
+
+
