78965211d629ee70c0a5829309ed7c4026b5d7f9
SP810 <2451142030@qq.com>
2022-05-26 20:23:29 +0800

    7——1更新

--- 7_1.py
+++ 7_1.py
@@ -74,20 +74,87 @@
 #7.2.3
 #使用标志：
 
+#标志很像我们之前的flag ，我们通过标志 作为循环条件，当我们的标志为假的时候则跳出循环。使代码更加简洁明了
+#我们将对之前的程序进行标志添加
 
+# promot="快写点东西给我测试用 "
+# promot+="\n哥们，来一个赖： "
+# message=""
+# active=True
+# while active:
+#  message=input(promot)
+#  if message=='quit':
+#      active=False
+#  else:
+#   print(f"{message} \n那个输入‘quit’就退出了\n")
 
+#7.2.4  跳出循环break
 
+# promot="快写点东西给我测试用 "
+# promot+="\n哥们，来一个赖： "
+# message=""
+# while True:
+#  message=input(promot)
+#  if message=='quit':
+#      break
+#  else:
+#   print(f"{message} \n那个输入‘quit’就退出了\n")
 
+#7.2.5  在循环中使用continue
 
+#打印奇数循环
 
+# count=0
+# while count<10:
+#     count+=1
+#     if count%2==0:
+#         continue
+#     print(count)
 
+#7.2.6 避免无限循环
+#记得避免哈哈哈，例子没用，单纯的提醒你
+#7.3 使用while循环出列列表和字典
 
+#7.3.1在列表之间移动元素
 
+# #首先，创建一个待验证用户列表
+# #和一个用于存储已验证用户的空列表
+# u_users=['daming','lihua','lili']
+# c_users=[]
+# #验证每个用户，直到没有未验证的用户为止
+# #将每个经过验证的用户  都移动到  已验证用户列表中
+# while u_users:
+#     c_user=u_users.pop()
+#     print(f"user is {c_user.title()}")
+#     c_users.append(c_user)
+# print("\n c_users are here")
+# for a in c_users:
+#     print(a.title())
+#     print(u_users)
 
+#7.3.2 删除为特定值的所有列表元素
+#我们使用的函数是第三章李的remove()
 
+# pets=['dogs','cat','dog','goldfish','cat','rabbit','cat']
+# print(pets)
+#
+# while 'cat' in pets:
+#     pets.remove('cat')
+# print(pets)
 
+# 7.3.3 使用用户输入来填充字典
 
-
-
-
+#设置一个标志，指出调查是否继续
+responses={}
+polling_active=True
+while polling_active:
+    name=input("\nWhat is your name: ")
+    response=input("你和谁是好朋友")
+    responses[name]=response
+    repeat=input("还有人要参与么么？ YES \ NO")
+    if repeat=='NO':
+        polling_active=False
+print("------程序的结果是--------")
+for name,response in responses.items():
+    print(f"{name} 的好朋友是{response}")
 
