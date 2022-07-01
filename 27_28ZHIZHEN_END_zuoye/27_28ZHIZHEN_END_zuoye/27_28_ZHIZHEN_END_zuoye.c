dc8373131c5e0a4609f8bdb25d08f96cf5c3467f
SP810 <2451142030@qq.com>
2022-07-01 10:35:16 +0800

    1

--- 27_28ZHIZHEN_END_zuoye/27_28ZHIZHEN_END_zuoye/27_28_ZHIZHEN_END_zuoye.c
+++ 27_28ZHIZHEN_END_zuoye/27_28ZHIZHEN_END_zuoye/27_28_ZHIZHEN_END_zuoye.c
@@ -204,38 +204,51 @@
 // 
 // 
 // 字符串左旋：
-void reverse(char* left, char* right)
-{
-	while (left < right)
-	{
-		char tmp = *left;
-		*left = *right;
-		*right = tmp;
-		left++;
-		right--;
-	}
-}
-void fun(char *arr,int k)
-{
-	//ABCDEF
-
-	int i = 0;
-	int j = 0;
-	int len=strlen(arr);
-	reverse(arr, arr + k - 1);//输入数字，控制字符串逆序
-	reverse(arr + k, arr + k - 1);
-	reverse(arr, arr + len - 1);
-	//char tmp = 0;
-	//for (j = 0; j < k%len; j++)//这里有优化  k--> k%len
-	//{
-	//	tmp = *arr;
-	//	for (i = 0; i < len - 1; i++)
-	//	{
-	//		*(arr + i) = *(arr + i + 1);
-	//	}
-	//	*(arr + len - 1) = tmp;
-	//}
-}
+//void reverse(char* left, char* right)
+//{
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//void fun(char *arr,int k)
+//{
+//	//ABCDEF
+//
+//	int i = 0;
+//	int j = 0;
+//	int len=strlen(arr);
+//	//法一：
+//	//reverse(arr, arr + k - 1);//输入数字，控制字符串逆序
+//	//reverse(arr + k, arr + len - 1);
+//	//reverse(arr, arr + len - 1);
+//	//法二：
+//	//char tmp = 0;
+//	//for (j = 0; j < k%len; j++)//这里有优化  k--> k%len
+//	//{
+//	//	tmp = *arr;
+//	//	for (i = 0; i < len - 1; i++)
+//	//	{
+//	//		*(arr + i) = *(arr + i + 1);
+//	//	}
+//	//	*(arr + len - 1) = tmp;
+//	//}
+//}
+// #include<string.h>
+//int main()
+//{
+//	char arr[10] = "ABCDEF";
+//	//左旋字符串
+//	int k = 0;
+//	scanf_s("%d", &k);
+//	fun(arr, k);
+//	printf("%s", arr);
+//	return 0;
+//}
 //杨氏矩阵
 // 要求时间复杂度<  (O)n
 // 就是说我们不能按照顺序一个一个查找，而是要根据矩阵的特点规律
@@ -243,57 +256,153 @@ void fun(char *arr,int k)
 //有一个数字矩阵，矩阵的每一行从左到右是递增的，矩阵从上到下是递增的
 //这里的规律是右上角为判断基本点，因为右上角是所在行最大的元素，所在列最小的元素
 //请编写程序再这样的矩阵中查找某个数是否存在
-void find_k(int arr[3][3], int k, int r, int c)
-{
-	int x = 0;
-	int y = c - 1;
-	int flag = 1;
-	//右上角的元素位置xy
-	//while(1)不严谨容易死循环
-	while (x<r&& y>=0)
-	{
-		if (arr[x][y] < k)
-		{
-			x = x + 1;
-		}
-		else if (arr[x][y] > k)
-		{
-			y = y - 1;
-		}
-		else
-		{
-			flag = 0;
-			printf("找到了哈哈哈，下标是%d %d\n", x+1, y+1);
-			break;
-		}
-	}
-	if(flag)
-	printf("没找着");
-}
-int main()
-{
-	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
-	int k = 11;
-	find_k(arr, k, 3, 3);
-
-	return 0;
-}
-//#include<string.h>
+//void find_k(int arr[3][3], int k, int r, int c)
+//{
+//	int x = 0;
+//	int y = c - 1;
+//	int flag = 1;
+//	int count = 0;
+//	//右上角的元素位置xy
+//	//while(1)不严谨容易死循环
+//	while (x<r&& y>=0)
+//	{
+//		count++;
+//		if (arr[x][y] < k)
+//		{
+//			x = x + 1;
+//		}
+//		else if (arr[x][y] > k)
+//		{
+//			y = y - 1;
+//		}
+//		else
+//		{
+//			flag = 0;
+//			printf("找到了哈哈哈，下标是%d %d\n", x+1, y+1);
+//			break;
+//		}
+//	}
+//	printf("时间复杂度为%d\n  ", count);
+//	if(flag)
+//	printf("没找着\n");
+//}
 //int main()
 //{
-//	char arr[10] = "ABCDEF";
-//	//左旋字符串
-//	int k = 0;
-//	scanf_s("%d",&k);
-//	fun(arr, k);
-//	printf("%s", arr);
+//	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
+//	int k = 11;
+//	find_k(arr, k, 3, 3);
+//
 //	return 0;
 //}
 // 
 // 
+//下面我们对程序做出一定的优化和改变
+//使得函数能够 返回 我们所想要的参数，x和y
+//int find_k(int arr[3][3], int k, int *pr, int *pc)
+////对函数的参数进行指针解引用，我们通常称之为返回型参数 
+//{
+//	int x = 0;
+//	int y = *pc - 1;
+//	int flag = 1;
+//	int count = 0;
+//	//右上角的元素位置xy
+//	//while(1)不严谨容易死循环
+//	while (x < *pr && y >= 0)
+//	{
+//		count++;
+//		if (arr[x][y] < k)
+//		{
+//			x = x + 1;
+//		}
+//		else if (arr[x][y] > k)
+//		{
+//			y = y - 1;
+//		}
+//		else
+//		{
+//			*pr = x+1;
+//			*pc = y+1;
+//			//printf("找到了哈哈哈，下标是%d %d\n", x + 1, y + 1);
+//			//break;
+//			return 0;
+//		}
+//	}
+//	printf("时间复杂度为%d\n  ", count);
+//	*pr = -1;
+//	*pc = -1;
+//}
+//int main()
+//{
+//	int arr[3][3] = { 1,2,3,4,5,6,7,8,9 };
+//	int k = 11;
+//	int x = 3;
+//	int y = 3;
+//	find_k(arr, k, &x, &y);
+//	if (x == -1 && y == -1)
+//	{
+//		printf("没找到\n");
+//	}
+//	else
+//	{
+//		printf("下标是%d %d\n", x, y); 
+//	}
+//	return 0;
+//}
+//我们在程序设计的过程中应当尽量使得我们的函数变得独立
+//这就是 高内聚 低耦合
 // 
 // 
-// // 
+//OJ题目
+//牛客网 BC100  
+//有序序列合并
+int main()
+{
+	int n, m;
+	scanf("%d%d", &n, &m);
+	int arr1[1000];
+	int arr2[1000];
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", arr1[i]);
+	}
+	for (i = 0; i < m; i++)
+	{
+		scanf("%d", arr2[i]);
+	}
+	//开始有序合并
+	i = 0;
+	j = 0;
+	while (i < n && j < m)
+	{
+		if (arr1[i] < arr2[j])
+		{
+			printf("%d ", arr1[i]);
+			i++;
+		}
+		else
+		{
+			printf("%d ", arr2[j]);
+			j++;
+		}
+	}
+	if (i == n)
+	{
+		for (; j < m; j++)
+		{
+			printf("%d ", arr2[j]);
+		}
+	}
+	else
+	{
+		for (; i < n; i++)
+		{
+			printf("%d ", arr2[i]);
+		}
+	}
+	return 0;
+}
+ 
 
 
 
